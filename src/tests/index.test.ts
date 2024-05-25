76b434cc72631411a2469f8528a7d34f6ddf82ad
pratikprajapati0708 <prajapatipratik292@gmail.com>
2024-05-25 09:26:20 +0530

    created test case for multiply

--- src/tests/index.test.ts
+++ src/tests/index.test.ts
@@ -1,8 +1,14 @@
 import {describe, expect, it, test} from '@jest/globals';
-import {sum} from '../index';
+import {multiply, sum} from '../index';
 
 describe('sum module', () => {
   it('adds 1 + 2 to equal 3', () => {
     expect(sum(1, 2)).toBe(3);
   });
 });
+
+describe('multiply',()=>{
+    it('multiply',()=>{
+        expect(multiply(3,4)).toBe(12);
+    })
+})
